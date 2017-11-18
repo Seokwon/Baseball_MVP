c4a34db1af2547c94da25e5180abded6a88e5387
Seokwon Jung <jsw4842@gmail.com>
2017-11-18 16:09:49 +0900

    branch test2

--- app/src/main/java/com/sw/baseball/baseball/MainPresenter.java
+++ app/src/main/java/com/sw/baseball/baseball/MainPresenter.java
@@ -71,7 +71,7 @@ public class MainPresenter {
 
         mView.layoutAddView(list());
 
-        Log.i("Result", "Result  : " + result[0] + " " + result[1] + " " + result[2] + " ");
+        Log.i("Result", "Result  : " + result[0] + " " + result[1] + " " + result[2] + "  테스트");
 
         String resultText;
         if (countS == 3) {
